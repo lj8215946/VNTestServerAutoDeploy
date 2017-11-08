46943b5394f675d5464c2691fc558c995b438bca
connorlu(芦佶) <connorlu@tencent.com>
2017-11-08 18:43:31 +0800

    提交autodeployvntestserver代码

---
+++ sync.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+echo -e "\033[32m [AUTO SYNC] sync start to cd \033[0m"
+
+cd /root/node.js/VNTestServer/VNTestServer
+
+echo -e "\033[32m [AUTO SYNC] sync start to pull \033[0m"
+
+git pull origin master 
+
